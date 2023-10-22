1a8026d47515e700d86878c4105851b4eb2e2c40
hunteryavitz <hunteryavitz@protonmail.com>
2023-10-22 22:54:18 +0000

    Deploying to gh-pages from @ hunteryavitz/blockchain-client-admin@49135df6590a50251e7bab6f3c75ebf3efbcce99 🚀

---
+++ assets/BlockchainAdminView-c5b96073.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as n}from"./index-1a9caf0c.js";const o={};function r(t,a){return e(),n("h1",null,"Blockchain Admin")}const _=c(o,[["render",r]]);export{_ as default};
---
+++ assets/ServiceAdminView-cc18e30c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-1a9caf0c.js";const r={};function o(t,s){return c(),n("h1",null,"Service Admin")}const a=e(r,[["render",o]]);export{a as default};
